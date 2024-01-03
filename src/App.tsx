59180fa99dfc3c84c4a272893c5e1b68795cf5e4
Marcos_Nascimento <marcozzymandias@gmail.com>
2024-01-03 09:49:18 -0300

    criando o front-end

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Card } from './components/card/card';
+import { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/card/create-modal/create-modal';
+
+function App() {
+  const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
+
+  return (
+      <div className='container'>
+          <h1>Cardapio</h1>
+          <div className='card-grid'>
+            {data?.map(foodData => 
+              <Card
+                price={foodData.price}
+                title={foodData.title}
+                image={foodData.image}
+              />
+            )}
+          </div>
+          {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+          <button onClick={handleOpenModal}>novo</button>
+      </div>
+  )
+}
+
+export default App
